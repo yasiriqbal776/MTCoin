3a89e9af7aad6d8131db2dd011e9127d6b63d795
yasiriqbal776 <yasiriqbal776@yahoo.com>
2017-12-03 02:01:51 +0500

    Push setting for genesis block

--- src/net.cpp
+++ src/net.cpp
@@ -1173,11 +1173,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"litecointools.com", "dnsseed.litecointools.com"},
-    {"litecoinpool.org", "dnsseed.litecoinpool.org"},
-    {"xurious.com", "dnsseed.ltc.xurious.com"},
-    {"koin-project.com", "dnsseed.koin-project.com"},
-    {"weminemnc.com", "dnsseed.weminemnc.com"},
+    {"litecointools.com", "169.56.130.115"},
     {NULL, NULL}
 };
 
